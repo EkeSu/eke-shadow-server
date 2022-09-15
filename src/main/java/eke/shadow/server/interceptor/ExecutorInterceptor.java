d86a7d8735f783e4ab06dcc176b029f2f7a20267
suyike <suyike@zhumanggroup.com>
2022-09-15 20:14:33 +0800

    新增拦截

--- src/main/java/eke/shadow/server/interceptor/ExecutorInterceptor.java
+++ src/main/java/eke/shadow/server/interceptor/ExecutorInterceptor.java
@@ -8,6 +8,7 @@ import eke.shadow.server.common.SupportType;
 import eke.shadow.server.config.DataSourceKey;
 import eke.shadow.server.config.DataSourceContextHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
@@ -16,6 +17,8 @@ import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -28,7 +31,11 @@ import java.util.regex.Pattern;
  * @Author：eke
  * @Date：2022/9/6
  */
-@Intercepts({@Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})})
+@Intercepts({
+        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class}),
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class})
+})
 @Component
 @Slf4j
 public class ExecutorInterceptor implements Interceptor {
